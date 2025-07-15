1db6e55cd3f9d872dc9f5ccc86c83f1e2f5954e8
bushrapatel28 <bpatel60@yahoo.com>
2025-07-15 17:13:25 +0000

    Add Canvas for Auth

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import routes from "tempo-routes";
 
 function App() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+    <Suspense fallback={<div>Loading...</div>}>
+      {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {import.meta.env.VITE_TEMPO && <Route path="/tempobook/*" />}
+      </Routes>
     </Suspense>
   );
 }
